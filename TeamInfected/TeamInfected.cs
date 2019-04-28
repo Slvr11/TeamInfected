eb1e2075b1dd03307476d9522aad4ae1a47a0290
Slvr99 <bsawii@gmail.com>
2019-04-27 20:03:03 -0400

    Add project files.

---
+++ TeamInfected/TeamInfected.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+namespace TeamInfected
+{
+    public class TeamInfected : BaseScript
+    {
+        public TeamInfected()
+        {
+            PlayerConnected += new Action<Entity>(entity =>
+            {
+                entity.Call("notifyonplayercommand", "teamtest", "+actionslot 3");
+                entity.OnNotify("teamtest", (player) =>
+                    {
+                            player.SetField("sessionteam", "axis");
+                    });
+                entity.SpawnedPlayer += () => OnPlayerSpawned(entity);
+            });
+        }
+        public void OnPlayerSpawned(Entity player)
+        {
+            TeamSwitch(player);
+        }
+        public void TeamSwitch(Entity player)
+        {
+            AfterDelay(1000, () => player.SetField("sessionteam", "axis"));
+        }
+    }
+}
